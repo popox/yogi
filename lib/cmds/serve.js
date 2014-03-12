popox/yogi

commit 5e0cae9713af142ef51d3a037f4fa68bf0b54225

Ignore all modules containing a build.json that are in the node_modules/ path

lib/cmds/serve.js
@@ -86,7 +86,7 @@ mods = {
         finder.on('path', function(file, stat) {
             if (stat.isFile()) {
                 var name = path.basename(file);
-                if (name === 'build.json') {
+                if (name === 'build.json' && !/node_modules\//.test(path.dirname(file))) {
                     mods.push(path.basename(path.dirname(file)));
                 }
             }

commit fb88eff19e1f62f05372ee0566f7cf24d7c685f5

Remove the pseudo patch that was wrong introduced in 5e0cae9

lib/cmds/serve.js
@@ -86,7 +86,7 @@ mods = {
         finder.on('path', function(file, stat) {
             if (stat.isFile()) {
                 var name = path.basename(file);
-                if (name === 'build.json' && !/node_modules\//.test(path.dirname(file))) {
+                if (name === 'build.json') {
                     mods.push(path.basename(path.dirname(file)));
                 }
             }